c87b959cbaa3fa695ba5670fc68dcd40f3b9cb21
wanaherbert <wanaherbert@gmail.com>
2022-12-30 11:03:08 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	charc;
+	for(c = 'a'; c <='z';c++)
+	{
+		putchar(c);
+	}
+_putchar('\n');
+}
+
