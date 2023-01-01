8e5703a1750f13d77f27c39512f3494014417023
wanaherbert <wanaherbert@gmail.com>
2023-01-01 06:53:38 -0800

    jan

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * main -Entry point
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,6 +17,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
 
