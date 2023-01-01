b1ac219bd7d43aab6e033f4e7017d61d83a14e2b
wanaherbert <wanaherbert@gmail.com>
2023-01-01 11:16:01 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,11 @@
-#include "main.hi"
+#include "main.h"
+
 /**
- * more_numbers - prints the numbers 0-14 ten times.
+ *  more_numbers - prints the numbers 0-14 ten times.
  */
 void more_numbers(void)
 {
+
 	int i, j;
 	for (i = 1; i <= 10; i++)
 	{
@@ -11,7 +13,7 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 				_putchar('1');
-		         	_putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
